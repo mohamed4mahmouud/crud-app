e66b8d7731f0afab04f71f2bf58b9d689c9062d0
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-02-09 20:03:38 +0200

    refactor create page

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@section('title','Form Create User')</title>
-</head>
+@extends('layouts.app')
+ @section('title','Form Create User')
 <body>
-    @extends('layouts.app')
+
     @section('content')
 
 
