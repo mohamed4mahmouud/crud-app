c5972fe1ca8ce7fa520e08cf503195cd8455ee61
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-02-10 16:00:47 +0200

    refactor show page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,35 +3,16 @@
 <body>
 
     @section('content')
-    <div class="container mt-4">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ $user->id }}</td>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>
-                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                        <form method="POST" action="{{ route('users.destroy', ['id' => $user->id]) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger m-2">Delete</button>
-                        </form>
-                        <a href="{{ route('users.show', ['id'=> $user->id]) }}" ><button class="btn btn-success m-2">Show</button></a>
-                        <a href="{{ route('users.edit', ['user' => $user->id]) }}" ><button class="btn btn-primary m-2">Edit</button></a>
-                    </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="row">
+  <div class="col-sm-6 mb-3 mb-sm-0">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ $user->name }}</h5>
+        <p class="card-text">{{ $user->email }}</p>
+        <a href="{{route('users.index')}}" class="btn btn-primary">Go Home</a>
+      </div>
     </div>
+  </div>
+</div>
 @endsection
 </body>
